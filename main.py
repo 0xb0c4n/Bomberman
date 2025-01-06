27c04eac9fea5d5153cf748a77ee023018a3eaef
0xb0c4n <womercode@gmail.com>
2025-01-06 17:26:17 +0100

    Push du day

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pyxel
 class App:
     def __init__(self):
         self.menu = Menu()
+        self.menuwin = MenuWin()
 
         self.grille = Grille(13,13)
         self.grille.position_init()
@@ -17,20 +18,21 @@ class App:
         self.player2.spawn()
 
         pyxel.init(208, 208, title="Bomberman", fps=30, display_scale=3)
+        pyxel.load("ressources.pyxres")
         pyxel.run(self.update, self.draw)
 
     def deplacement(self):
         for k in pyxel.__dict__.keys():
             if k.startswith('KEY_'):
-                if pyxel.btnp(getattr(pyxel, k)):
+                if pyxel.btn(getattr(pyxel, k)):
                    for elt in DB.KEYS:
                        if k in elt:
                         player = elt.index(k)
                         direction = DB.KEYS[elt]
                         if player == 0:
-                            self.player1.goto(direction)
+                            self.player1.goto_fluid(direction)
                         else:
-                            self.player2.goto(direction)
+                            self.player2.goto_fluid(direction)
 
     def bombarder(self):
         if pyxel.btnp(pyxel.KEY_E):
@@ -46,15 +48,15 @@ class App:
                 if case.bomber != []:
                     for player in case.bomber:
                         if not(player.dead):
-                            pyxel.rect(16*i, 16*j, 16, 16, player.id)
+                            pyxel.rect(player.move_x, player.move_y, 16, 16, player.id)
 
                 if case.bomb != None:
                     pyxel.rect(16*i, 16*j, 16,16 , 3)
 
                 if case.terrain == PARAMS.PILLIER:
-                    pyxel.rect(16*i, 16*j, 16, 16, 0)
+                    pyxel.blt(16*i, 16*j,0, 48, 48, 16, 16)
                 elif case.terrain == PARAMS.BRIQUE:
-                    pyxel.rect(16*i, 16*j, 16, 16, 13)
+                    pyxel.blt(16*i, 16*j, 0, 64, 48, 16, 16)
 
     def update(self):
         #Quit window
@@ -70,8 +72,10 @@ class App:
         if self.menu.show:
             self.menu.update()
             self.menu.draw()
+        elif self.menuwin.show:
+            self.menuwin.draw()
         else:
-            pyxel.cls(7)
+            pyxel.cls(0)
             self.draw_grille()
 
 App()
--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import pyxel
 
 class Menu:
     def __init__(self):
-        self.show = True
+        self.show = False
         self.i = 0
 
     def update(self):
@@ -14,7 +14,16 @@ class Menu:
     def draw(self):
         pyxel.cls(0)
         pyxel.text(65,20,"Bomberman by 0xb0c4n", pyxel.frame_count % 12)
-        pyxel.text(80,40,"Solo", 7)
-        pyxel.text(80,60,"1v1", 7)
-        pyxel.text(80,80,"Multiplayer", 7)
+        pyxel.text(80,40,"Play", 7)
+        pyxel.text(80,60,"Options", 7)
+        pyxel.text(80,80,"Quit", 7)
         pyxel.tri(70,39+20*self.i,75,42+20*self.i,70,45+20*self.i,7)
+
+class MenuWin:
+    def __init__(self, win_player=""):
+        self.show = False
+        self.win_player = win_player
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(65,20,self.win_player + " wins !", pyxel.frame_count % 12)
